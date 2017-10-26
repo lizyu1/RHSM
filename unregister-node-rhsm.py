f764fcc0dfbbda2adf0b2523ff9a95c014816efa
Liz Yu <liz.yu123@gmail.com>
2017-10-26 21:57:37 +1100

    Initial commit

---
+++ unregister-node-rhsm.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+# 23-10-2017 liz
+
+import json
+import sys
+
+
+try:
+    import requests
+except ImportError:
+    print "Please install the python-requests module."
+    sys.exit(-1)
+
+USERNAME = "username"
+PASSWORD = "password"
+# Ignore SSL for now
+SSL_VERIFY = False
+
+
+def main(hostname):
+    """
+    Performs a GET using the passed URL location
+    """
+
+    r = requests.get('https://url/api/v2/hosts/{0}'.format(hostname), auth=(USERNAME, PASSWORD), verify=SSL_VERIFY)
+
+    print r.text
+    d = json.loads(r.content)
+    id = d["id"]
+    print id
+
+if __name__ == "__main__":
+  if len(sys.argv) == 2:
+        main(sys.argv[1])
+    else:
+        scriptname = sys.argv[0].split('/')[:-1]
+        print("Usage: {0} <hostname>".format(scriptname))
